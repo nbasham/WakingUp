ad9296a095de83126a64e98feb606d2fed5cd901
Norman Basham <2135673+nbasham@users.noreply.github.com>
2023-12-16 10:38:21 -0800

    Initial Commit

---
+++ WakingUp/WakingUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakingUpApp.swift
+//  WakingUp
+//
+//  Created by Norman Basham on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct WakingUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
