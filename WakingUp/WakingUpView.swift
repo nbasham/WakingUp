a33e920fb1975584b35f738a21c4ac157c64715c
Norman Basham <2135673+nbasham@users.noreply.github.com>
2023-12-16 11:41:44 -0800

    Don't like the approach to truncate title.

--- WakingUp/WakingUpView.swift
+++ WakingUp/WakingUpView.swift
@@ -7,12 +7,20 @@ struct WakingUpView: View {
         ScrollView {
             ForEach(models) { model in
                 HStack {
-                    Text(model.title)
+                    GeometryReader { geometry in
+                        Text(model.title)
+                            .frame(width: geometry.size.width, alignment: .leading)
+                            .lineLimit(1)
+                            .truncationMode(.tail)
+                    }
                     Spacer()
-                    Text(model.date.formatted(date: .abbreviated, time: .omitted))
+                    Text(model.date.formatted(date: .numeric, time: .omitted))
+                        .fixedSize()
                     Spacer()
                     Text("\(model.duration)")
+                        .fixedSize()
                 }
+                .lineLimit(1)
             }
         }
         .padding()
